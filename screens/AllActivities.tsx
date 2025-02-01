a09a2f37fc557619e940babdefe3bd2220dc6534
Carlos <edcartech@gmail.com>
2025-01-31 23:52:52 -0800

    refactor AllActivities screen to support theme context and add items list

--- screens/AllActivities.tsx
+++ screens/AllActivities.tsx
@@ -1,14 +1,43 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { useContext } from "react";
+import { ThemeContext } from "../ThemeContext";
+import { TYPOGRAPHY, LAYOUT } from "../constants/styles";
+import ItemsList from "../components/ItemsList";
 
 interface AllActivitiesProps {
   onAdd: () => void;
 }
 
 export default function AllActivities({ onAdd }: AllActivitiesProps) {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <View testID="all-activities-view">
-      <Text testID="all-activities">All Activities</Text>
+    <View
+      testID="all-activities-view"
+      style={[styles.container, { backgroundColor: theme.backgroundColor }]}
+    >
+      <Text
+        testID="all-activities"
+        style={[
+          styles.title,
+          { color: theme.textColor }
+        ]}
+      >
+        All Activities
+      </Text>
+      <Button title="Add" onPress={onAdd} />
+      <ItemsList type="activities" />
     </View>
   );
 }
-const styles = StyleSheet.create({});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  title: {
+    ...TYPOGRAPHY.TITLE,
+    textAlign: 'center',
+    marginVertical: LAYOUT.MARGIN,
+  },
+});
