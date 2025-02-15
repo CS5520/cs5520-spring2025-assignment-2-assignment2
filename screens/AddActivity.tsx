ffd19f6ab1e8d30607df5315f57df95520d909a1
Yan Wang <wang.yan8@northeastern.edu>
2025-02-14 22:33:40 -0800

    Add activity and diet input forms with validation and theme support

--- screens/AddActivity.tsx
+++ screens/AddActivity.tsx
@@ -1,5 +1,18 @@
-import { View, Text, StyleSheet, Alert, Button } from "react-native";
-
+import { View, Text, StyleSheet, Button, Alert } from "react-native";
+import  { useState } from "react";
+import DropDownPicker from 'react-native-dropdown-picker';
+import { writeToDB } from "@/firebase/firestore";
+import Input from "@/components/Input"; 
+import DatePicker from "@/components/DatePicker";
+import { useContext } from "react";
+import { ThemeContext } from "@/ThemeContext";
+import {
+  color,
+  commonTitleStyles, 
+  commonButtonContainerStyles,
+  commonTextStyles,
+} from "@/constants/styles";
+ 
 import { Timestamp } from "firebase/firestore";
 
 export interface Activity {
@@ -8,15 +21,131 @@ export interface Activity {
   date: Timestamp;
   important: boolean;
 }
+
 interface AddActivityProps {
   onSave: () => void;
 }
+
 export default function AddActivity({ onSave }: AddActivityProps) {
+  const { theme } = useContext(ThemeContext);
+  const [activity, setActivity] = useState("");
+  const [duration, setDuration] = useState("");
+  const [date, setDate] = useState<Date | undefined>(undefined);
+  const [open, setOpen] = useState(false);
+  const [items, setItems] = useState([
+    { label: "Walking", value: "Walking" },
+    { label: "Running", value: "Running" },
+    { label: "Swimming", value: "Swimming" },
+    { label: "Weights", value: "Weights" },
+    { label: "Yoga", value: "Yoga" },
+    { label: "Cycling", value: "Cycling" },
+    { label: "Hiking", value: "Hiking" },
+  ]);
+
+  // Validate inputs
+  const validateInputs = (): boolean => {
+    if (!activity || !duration || !date) {
+      Alert.alert("Invalid Input", "Please check and fill in all required fields and try again.");
+      return false;
+    }
+    if (isNaN(Number(duration)) || Number(duration) <= 0) {
+      Alert.alert("Invalid Input", "Duration must be a positive number. Please Check");
+      return false;
+    }
+    return true;
+  };
+ 
+  function handleSave() {
+    if (!validateInputs()) {
+      return;
+    }
+
+    const important = (activity === "Running" || activity === "Weights")
+                       && Number(duration) > 60;
+    
+    const newActivity: Activity = {
+      activity: activity,
+      duration: duration,
+      date: Timestamp.fromDate(date as Date),
+      important: important,
+    };
+    writeToDB("activities", newActivity);
+    onSave();
+  }
+
   return (
-    <View testID="add-activity-view">
-      <Text testID="add-activity">Add Diet</Text>
-    </View>
+    <View 
+      testID="add-activity-view" 
+      style={[styles.container, { backgroundColor: theme.backgroundColor }]} 
+    >
+      <Text 
+        testID="add-activity" 
+        style={[commonTitleStyles.title, styles.title, { color: theme.textColor }]}>
+      Add Activity
+      </Text>
+      <View style={styles.pickerContainer}>
+        <Text 
+          style={[commonTextStyles.text, { color: theme.textColor }]}>
+        Activity*
+        </Text> 
+        <DropDownPicker
+          open={open}
+          value={activity}
+          items={items}
+          setOpen={setOpen}
+          setValue={setActivity}
+          setItems={setItems}
+          placeholder="Select Activity"
+          testID="dropdown-picker"
+          style={[styles.picker, 
+            { backgroundColor: theme.backgroundColor === color.tertiaryColor ?
+            color.white : color.primaryColor }]}
+          containerStyle={styles.pickerContainerStyle}
+        />
+      </View>
+      
+      <View>
+        <Input 
+          label="Duration(min)*"
+          placeholder="Enter duration"
+          value={duration}
+          onChangeText={setDuration}
+        />
+      </View>
+      
+      <DatePicker
+        date={date}
+        onDateChange={setDate}
+      />
+      
+      <View style={[commonButtonContainerStyles.buttonContainer,styles.buttonContainer]}>
+        <Button title="Cancel" onPress={onSave} />
+        <Button title="Save" onPress={handleSave} />
+      </View>    
+    </View>   
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: color.primaryColor,
+    flex: 1,
+  },
+  title: {
+    marginBottom: 40,
+  },
+  picker: {
+    backgroundColor: color.primaryColor,
+  },
+  pickerContainer: {
+    marginLeft: 10,
+  },
+  buttonContainer: {
+    justifyContent: 'space-around',
+    marginTop: 40,
+  },
+  pickerContainerStyle: {
+    width: '95%',
+  }
+});
+
--- screens/AddDiet.tsx
+++ screens/AddDiet.tsx
@@ -1,5 +1,12 @@
-import { View, Text, StyleSheet, Alert, Button } from "react-native";
+import { View, Text, StyleSheet, Button, Alert } from "react-native";
+import { useState } from "react";
 import { Timestamp } from "firebase/firestore";
+import Input from "@/components/Input";
+import { writeToDB } from "@/firebase/firestore";
+import DatePicker from "@/components/DatePicker";
+import { useContext } from "react";
+import { ThemeContext } from "@/ThemeContext";
+import { color, commonTitleStyles, commonButtonContainerStyles } from "@/constants/styles";
 
 export interface Diet {
   calories: string;
@@ -7,15 +14,94 @@ export interface Diet {
   date: Timestamp;
   important: boolean;
 }
+
 interface AddDietProps {
   onSave: () => void;
 }
+
 export default function AddDiet({ onSave }: AddDietProps) {
+  const { theme } = useContext(ThemeContext);
+  const [description, setDescription] = useState("");
+  const [calories, setCalories] = useState("");
+  const [date, setDate] = useState<Date | undefined>(undefined);
+  
+  // Validate inputs
+  const validateInputs = (): boolean => {
+    if (!description || !calories|| !date) {
+      Alert.alert("Invalid Input", "You have missed some information. Please check");
+      return false;
+    }
+    if (isNaN(Number(calories)) || Number(calories) < 0) {
+      Alert.alert("Invalid Input", "Calories should be a positive number. Please check");
+      return false;
+    }
+    return true;
+  };
+
+  function handleSave() {
+    if (!validateInputs()) {
+      return;
+    }
+
+    const important = ( Number(calories) > 800 ) ? true : false;
+    const newDiet: Diet = {
+      description,
+      calories,
+      date: Timestamp.fromDate(date as Date),
+      important,
+    };
+    writeToDB("diets", newDiet);
+    onSave();
+  }
+
   return (
-    <View testID="add-diet-view">
-      <Text testID="add-diet">Add Diet</Text>
+    <View 
+      testID="add-diet-view" 
+      style={[styles.container, { backgroundColor: theme.backgroundColor }]}
+    >
+      <Text 
+        testID="add-diet" 
+        style={[commonTitleStyles.title, styles.title, { color: theme.textColor }]}>
+      Add Diet
+      </Text>
+      <Input 
+        label="Description*"
+        placeholder="Enter description"
+        value={description}
+        onChangeText={setDescription} 
+      />
+      <Input
+        label="Calories*"
+        placeholder="Enter calories"
+        value={calories}
+        onChangeText={setCalories}
+      />
+      <DatePicker 
+        date={date} 
+        onDateChange={setDate} 
+      />
+      <View 
+        style={[commonButtonContainerStyles.buttonContainer, styles.buttonContainer]}
+      >
+        <Button title="Cancel" onPress={onSave} />
+        <Button title="Save" onPress={handleSave} />
+      </View>
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: color.primaryColor,
+    flex: 1,
+  },
+  title: {
+    marginBottom: 40,
+  },
+  buttonContainer: {
+    justifyContent: 'space-around',
+    marginTop: 40,
+  },
+});
+
